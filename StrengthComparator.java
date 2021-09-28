26b29d12c149143b719f55e4db5ae3b6138223e1
KisRusi <83890330+KisRusi@users.noreply.github.com>
2021-09-28 22:13:07 +0200

    Add files via upload

---
+++ StrengthComparator.java
@@ -0,0 +1,13 @@
+import java.util.Comparator;
+
+public class StrengthComparator implements Comparator<Beer>{
+	@Override
+	public int compare(Beer t, Beer t1) {
+		if(t.strength==t1.strength)
+			return 0;
+		else if(t.strength<t1.strength)
+			return -1;
+		else 
+			return 1;
+	}
+}
