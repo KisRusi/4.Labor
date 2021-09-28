c12d8d1cb7473f30489e22acb68ed542621cc701
KisRusi <83890330+KisRusi@users.noreply.github.com>
2021-09-28 22:12:22 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,13 @@
 import java.util.Scanner;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Collections;
 public class Main {
 	static ArrayList<Beer> beers = new ArrayList<Beer>();
 	protected static void add(String[] cmd)
@@ -8,14 +16,84 @@ public class Main {
 		beers.add(uj);
 	}
 	
-	protected static void list()
+	protected static void list(String[] cmd)
 	{
+		
+		Comparator<Beer> cmp = null;
+		switch(cmd[1])
+		{
+		case "name":
+			cmp = new NameComparator();
+			break;
+		case "style":
+			cmp = new StyleComparator();
+			break;
+		case "strength":
+			cmp = new StrengthComparator();
+			break;
+		}
+		Collections.sort(beers, cmp);
+		
 		for(Beer sorok : beers)
 		{
 			System.out.println(sorok);
+			
 		}
 	}
-	public static void main(String[] args) {
+	
+	protected static void save() throws IOException, ClassNotFoundException
+	{
+		FileOutputStream fileout = new FileOutputStream("BeersInfo.ser");
+		ObjectOutputStream out = new ObjectOutputStream(fileout);
+		out.writeObject(beers);
+		out.close();
+		fileout.close();
+
+	}
+	
+	protected static void load() throws IOException, ClassNotFoundException
+	{
+		ArrayList<Beer> beers = null;
+		FileInputStream fileIn = 	new FileInputStream("C:\\Users\\rusva\\workspace\\4.labor\\BeersInfo.ser");
+		ObjectInputStream in = new ObjectInputStream(fileIn);
+		beers = (ArrayList<Beer>) in.readObject();
+		in.close();
+		fileIn.close();
+	}
+	protected static void  search(String name)
+	{
+		for(Beer sorok : beers)
+		{
+			if(sorok.getName().matches(name))
+			{
+				System.out.println(sorok);
+			}
+			
+			
+		} 
+	}
+	protected static void find(String smth)
+	{
+		for(Beer sorok : beers)
+		{
+			if(sorok.getName().contains(smth));
+			{
+				System.out.println(sorok);
+			}
+		}
+	}
+	/*protected static void delete(String name)
+	{
+		for(Beer sorok : beers)
+		{
+			if(sorok.getName().matches(name))
+			{
+				
+				System.out.println("Az elem torolve");
+			}
+		}
+	}*/
+	public static void main(String[] args) throws ClassNotFoundException, IOException {
 		
 
 		Scanner scanner = new Scanner(System.in);
@@ -34,7 +112,23 @@ public class Main {
 			}
 			else if (cmd[0].equals("list"))
 			{
-				list();
+				list(cmd);
+			}
+			else if(cmd[0].equals("save"))
+			{
+				save();
+			}
+			else if(cmd[0].equals("load"))
+			{
+				load();
+			}
+			else if(cmd[0].equals("search"))
+			{
+				search(cmd[1]);
+			}
+			else if(cmd[0].equals("find"))
+			{
+				find(cmd[1]);
 			}
 			else if ( cmd[0].equals("exit"))
 			{
